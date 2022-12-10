668452c8bd75f4a00983e1b298bb0ec3355616d9
Takahiro Koshiba <koshiba@h142.14.244.10.1017135.vlan.kuins.net>
2022-12-10 16:56:51 +0900

    paramEstimateGumbel is created

---
+++ src/paramEstimateGumbel.m
@@ -0,0 +1,38 @@
+function [paramHat] = paramEstimateGumbel(d)
+% PARAMESTIMATEGUMBEL
+% estimate parameters of Gumbel (mu, sigma)
+% paramHat(1) is mu
+% paramHat(2) is sigma
+% This function uses PWM. 
+% For Gumbel, MLE or PWM are recommended (c.f. エース水文学).
+
+% common values for parameter estimate
+dMean = mean(d);
+% dStd     = std(d);
+GAMMA = 0.57721566490153286060651209008240243104215933593992;
+N     = length(d);
+
+% MLE(極値統計学（高橋）p.)
+
+
+% PWM(極値統計学（高橋）p.110)
+di     = sort(d);
+alpha0 = dMean;
+% alpha1 = sum((1:N-1) .* di(2:end)) / N / N-1;
+alpha1 = sum(((1:N) - 0.35) .* di(1:end)) / N^2;
+sigmaHat   = (2*alpha1 - alpha0) / log(2); 
+if sigmaHat < 0; sigmaHat = NaN; end % sigma must be positive.
+muHat      = alpha0 - GAMMA*sigmaHat;       
+paramHat   = [muHat, sigmaHat];
+
+    
+% plot
+% mu = paramHat(1);
+% sigma = paramHat(2);
+% p = @(x) exp(-(x-mu)/sigma - exp(-(x-mu)/sigma)) / sigma;
+% histogram(d, 1.5:0.1:6);
+% y = linspace(1.5, 6, 1001);
+% pp = p(y);
+% yyaxis right
+% plot(y, pp, 'LineWidth', 2)
+end
